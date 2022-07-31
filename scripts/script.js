530860664e275315957f267906b78b5af3a15ba0
Nello Castello <dfarino46@gmail.com>
2022-07-31 13:35:59 +0300

    реализовано: показывать и скрывать правое меню

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let rightMenu = document.querySelector(".right-menu");
+
+function openRightMenu() {
+    rightMenu.classList.add("--active")
+}
+
+function closeRightMenu() {
+    rightMenu.classList.remove("--active")
+}
